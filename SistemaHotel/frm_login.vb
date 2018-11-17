f4cd14ff6d341e3de0099d29fc24e43913e3c334
ArthurMVilela <arthuramrinheirovilela@gmail.com>
2018-11-17 20:55:09 -0200

    Menu login

--- SistemaHotel/frm_login.vb
+++ SistemaHotel/frm_login.vb
@@ -7,10 +7,31 @@
     End Sub
 
     Private Sub btn_entrar_Click(sender As Object, e As EventArgs) Handles btn_entrar.Click
-        usuario = validarUsuario(txt_nome.Text, txt_senha.Text)
+
+        If txt_nome.Text = "" Or txt_senha.Text = "" Then
+            MsgBox("Preencha todos os campos!", MsgBoxStyle.Exclamation, "ATENÇÃO")
+            Exit Sub
+        Else
+            usuario = validarUsuario(txt_nome.Text, txt_senha.Text)
+        End If
+
 
         If usuario.id <> -1 Then
             Me.Close()
+        Else
+            lbl_info.ForeColor = Color.Red
+            If getErroLogin() = 1 Then
+                lbl_info.Text = "Este usuário não existe."
+                txt_nome.Text = ""
+                txt_senha.Text = ""
+                txt_nome.Select()
+
+            ElseIf getErroLogin() = 2 Then
+                lbl_info.Text = "Senha inválida."
+                txt_senha.Text = ""
+                txt_senha.Select()
+
+            End If
         End If
     End Sub
 End Class
